97cad28370c871dda49c0c5af9108f8ce6e53620
Natanael Arndt <arndtn@gmail.com>
2026-01-02 12:29:44 +0100

    Add description and arguments for function

--- functions/ocrpdf.fish
+++ functions/ocrpdf.fish
@@ -1,3 +1,3 @@
-function ocrpdf --wraps "ocrmypdf --clean --rotate-pages --force-ocr --output-type pdf'"
-  ocrmypdf --clean --rotate-pages --force-ocr --output-type pdf
+function ocrpdf --wraps "ocrmypdf --clean --rotate-pages --force-ocr --output-type pdf'" --description "OCR a PDF document" --argument-names input_pdf_or_image output_pdf
+  ocrmypdf --clean --rotate-pages --force-ocr --output-type pdf $input_pdf_or_image $output_pdf
 end
